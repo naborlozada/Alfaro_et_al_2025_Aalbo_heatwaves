254d9585d8a6a4e2c34c69be7c39c9fe20612aee
Nabor Lozada <35967936+naborlozada@users.noreply.github.com>
2025-06-29 01:04:12 +0200

    Update and rename rna-seq_command.txt to rna-seq_command.sh

--- scripts/rna-seq_command.sh
+++ scripts/rna-seq_command.sh
@@ -3,4 +3,7 @@ time nextflow run nf-core/rnaseq -r 3.17.0 \
     --fasta /home/carlassara/Claudia/RefGenome/GCF_035046485.1_AalbF5_genomic_clean.fna.fasta \
     --gtf /home/carlassara/Claudia/RefGenome/GCF_035046485.1_AalbF5_genomic.gtf \
     --outdir /home/carlassara/Claudia/Results/Corrected/ \
-    -profile singularity --pseudo_aligner salmon --max_cpus 4 --max_memory 64.GB --bam_csi_index --skip_dupradar false --extra_salmon_quant_args '--seqBias --gcBias' 2>&1 | tee nextflow.nfcore_rnaseq.claudias_heatwaves.extended_functions.stderr.v08112024.log
+    -profile singularity --pseudo_aligner salmon \
+    --max_cpus 4 --max_memory 64.GB \
+    --bam_csi_index --skip_dupradar false \
+    --extra_salmon_quant_args '--seqBias --gcBias' 2>&1 | tee nextflow.nfcore_rnaseq.claudias_heatwaves.extended_functions.stderr.v08112024.log
