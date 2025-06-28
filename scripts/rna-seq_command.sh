da2a7bbeb9649110f5d859027a6dc81e63ad4f4b
Nabor Lozada <35967936+naborlozada@users.noreply.github.com>
2025-06-29 01:42:29 +0200

    Update rna-seq_command.sh

--- scripts/rna-seq_command.sh
+++ scripts/rna-seq_command.sh
@@ -1,6 +1,7 @@
-# info:
-# N E X T F L O W   ~  version 24.10.0
-# Launching `https://github.com/nf-core/rnaseq` [berserk_goodall] DSL2 - revision: 00f924cf92 [3.17.0] 
+## info:
+## run it in a background 'screen' linux session. 
+## N E X T F L O W   ~  version 24.10.0
+## Launching `https://github.com/nf-core/rnaseq` [berserk_goodall] DSL2 - revision: 00f924cf92 [3.17.0] 
 
 time nextflow run nf-core/rnaseq -r 3.17.0 \
     --input /home/alfaro/infiles/input_table.csv \
@@ -11,3 +12,5 @@ time nextflow run nf-core/rnaseq -r 3.17.0 \
     --max_cpus 4 --max_memory 64.GB \
     --bam_csi_index --skip_dupradar false \
     --extra_salmon_quant_args '--seqBias --gcBias' 2>&1 | tee nextflow.nfcore_rnaseq.claudias_heatwaves.extended_functions.stderr.v08112024.log
+
+    # end
