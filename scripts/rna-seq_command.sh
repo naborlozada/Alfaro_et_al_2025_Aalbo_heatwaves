9d871fa3bcc5b517178c69dafed4088f9500e7c7
Nabor Lozada <35967936+naborlozada@users.noreply.github.com>
2025-07-03 08:13:05 +0200

    Update rna-seq_command.sh

--- scripts/rna-seq_command.sh
+++ scripts/rna-seq_command.sh
@@ -7,7 +7,7 @@ time nextflow run nf-core/rnaseq -r 3.17.0 \
     --input /home/alfaro/infiles/input_table.csv \
     --fasta /home/alfaro/RefGenome/GCF_035046485.1_AalbF5_genomic_clean.fna.fasta \
     --gtf /home/alfaro/RefGenome/GCF_035046485.1_AalbF5_genomic.gtf \
-    --outdir /home/Claudia/Results/BiasCorrection/ \
+    --outdir /home/alfaro/Results/BiasCorrection/ \
     -profile singularity --pseudo_aligner salmon \
     --max_cpus 4 --max_memory 64.GB \
     --bam_csi_index --skip_dupradar false \
